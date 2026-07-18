566655eac25c03ed207375686d64539bf5e6e277
yc0023861-byte <yc0023861@gmail.com>
2026-07-18 13:34:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-// WorldwideAuraRugs Business Suite
+function calculate() {
 
-console.log("WorldwideAuraRugs Business Suite Loaded");
+let l = parseFloat(document.getElementById("length").value);
+let w = parseFloat(document.getElementById("width").value);
+let h = parseFloat(document.getElementById("height").value);
+let actual = parseFloat(document.getElementById("weight").value);
+let courier = document.getElementById("courier").value;
 
-function showMessage(message) {
-    alert(message);
+if(isNaN(l) || isNaN(w) || isNaN(h) || isNaN(actual)){
+    alert("Please fill all fields");
+    return;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Dashboard Ready");
-});
+let divisor = 5000;
+
+if(courier=="DHL") divisor=5000;
+if(courier=="FedEx") divisor=5000;
+if(courier=="UPS") divisor=5000;
+
+let volumetric=(l*w*h)/divisor;
+
+let chargeable=Math.max(actual,volumetric);
+
+document.getElementById("result").innerHTML=
+`
+<b>Courier:</b> ${courier}<br><br>
+
+📦 Volumetric Weight :
+<b>${volumetric.toFixed(2)} kg</b>
+
+<br><br>
+
+⚖ Chargeable Weight :
+<b>${chargeable.toFixed(2)} kg</b>
+`;
+
+}
