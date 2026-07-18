0c1f6e2ff85a22ea585a654456b71e67d695e870
yc0023861-byte <yc0023861@gmail.com>
2026-07-18 14:45:40 +0530

    Update countries.js

--- countries.js
+++ countries.js
@@ -34,3 +34,25 @@ function loadCountries(selectId) {
     select.appendChild(option);
   });
 }
+const shippingRates = {
+  "India": 12,
+  "United States": 35,
+  "United Kingdom": 30,
+  "Canada": 32,
+  "Australia": 38,
+  "Germany": 28,
+  "France": 28,
+  "Italy": 29,
+  "Spain": 29,
+  "Netherlands": 27,
+  "Belgium": 27,
+  "Sweden": 31,
+  "Norway": 33,
+  "Denmark": 30,
+  "Switzerland": 34,
+  "Austria": 29,
+  "Ireland": 30,
+  "Japan": 36,
+  "Singapore": 24,
+  "United Arab Emirates": 20
+};
