288e17819c338d7f2856289944ffc0fdac82a7d3
yc0023861-byte <yc0023861@gmail.com>
2026-07-18 13:27:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// WorldwideAuraRugs Business Suite
+
+console.log("WorldwideAuraRugs Business Suite Loaded");
+
+function showMessage(message) {
+    alert(message);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Dashboard Ready");
+});
